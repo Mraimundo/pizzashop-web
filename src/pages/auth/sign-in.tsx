2cd3912086d7165d4ee2f2a1908c26c99050d018
Mraimundo <mdumundo18@gmail.com>
2024-03-21 15:49:35 -0300

    feat: add sign up page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const signInSchema = z.object({
   email: z.string().email(),
@@ -43,6 +44,9 @@ export function SignIn() {
       <Helmet titleTemplate="Login" />
 
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to={"/sign-up"}>Novo estabelecimento</Link>
+        </Button>
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
